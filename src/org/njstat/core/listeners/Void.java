0c2a70dbc74631aa634264ace1092ab04274e6a7
BuildTools <unconfigured@null.spigotmc.org>
2016-12-13 18:13:10 -0500

    Fixed

--- src/org/njstat/core/listeners/Void.java
+++ src/org/njstat/core/listeners/Void.java
@@ -1,5 +1,18 @@
 package org.njstat.core.listeners;
 
-public class Void {
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class Void implements Listener{
+	
+	@EventHandler
+	public void voidfall(PlayerMoveEvent event){
+		Player player = (Player) event.getPlayer();
+		if (player.getLocation().getY() < 9) {
+            player.performCommand("spawn");
+        }
+	}
 
 }
