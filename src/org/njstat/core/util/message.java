54d239d0f2a08974ac46690129ea95e5e6663f80
BuildTools <unconfigured@null.spigotmc.org>
2016-12-14 17:49:43 -0500

    Updated color

--- src/org/njstat/core/util/message.java
+++ src/org/njstat/core/util/message.java
@@ -4,19 +4,19 @@ import net.md_5.bungee.api.ChatColor;
 
 public interface message {
 	
-	String GAMEMODE_CREATIVE = "Gamemode set to creative";
-	String GAMEMODE_SURVIVAL = "Gamemode set to survival";
-	String CORE = "Core is running version";
-	String CORE_VERISON = "beta3 [December 14 2016]";
-	String CORE_IS_STAFF = "You are staff";
+	String GAMEMODE_CREATIVE = ChatColor.translateAlternateColorCodes('&', "Gamemode set to &acreative");
+	String GAMEMODE_SURVIVAL = ChatColor.translateAlternateColorCodes('&', "Gamemode set to &asurvival");
+	String CORE = ChatColor.translateAlternateColorCodes('&', "Core is running version ");
+	String CORE_VERISON = ChatColor.translateAlternateColorCodes('&', "&abeta3 &7[December 14 2016]");
+	String CORE_IS_STAFF = ChatColor.translateAlternateColorCodes('&', "You are staff");
 	
-	String NOPERMISSION = "No permission";
-	String NOTONLINE = "That player is not online";
+	String NOPERMISSION = ChatColor.translateAlternateColorCodes('&', "&cNo permission");
+	String NOTONLINE = ChatColor.translateAlternateColorCodes('&', "&cThat player is not online");
 	
-	String SETRANK_USAGE = "/setrank <player> <rank>";
+	String SETRANK_USAGE = ChatColor.translateAlternateColorCodes('&', "/setrank <player> <rank>");
 	
-	String SPAWN_SET = "Spawnpoint set in ";
-    String SPAWN = "Sent to spawn";
+	String SPAWN_SET = ChatColor.translateAlternateColorCodes('&', "Spawnpoint set in ");
+    String SPAWN = ChatColor.translateAlternateColorCodes('&', "Sent to spawn");
     
     String SERVER_SELECTOR_NAME = ChatColor.translateAlternateColorCodes('&', "&aServer Selector");
     
